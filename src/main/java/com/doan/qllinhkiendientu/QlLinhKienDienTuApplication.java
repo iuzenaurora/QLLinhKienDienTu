0874cce9af91d46ca9d10ebc23e847c5eceec858
Aurora Zen <nuyenphat2468@gmail.com>
2026-04-26 16:25:23 +0700

    Add database

---
+++ src/main/java/com/doan/qllinhkiendientu/QlLinhKienDienTuApplication.java
@@ -0,0 +1,13 @@
+package com.doan.qllinhkiendientu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlLinhKienDienTuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlLinhKienDienTuApplication.class, args);
+    }
+
+}
