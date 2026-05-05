e50845f541b8d46a3ad3aa0731f6db9b2ce3cf1d
Aurora Zen <nuyenphat2468@gmail.com>
2026-05-05 22:25:19 +0700

    update templates

---
+++ src/main/java/com/doan/qllinhkiendientu/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.doan.qllinhkiendientu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("pageTitle", "Trang chủ");
+        return "index";
+    }
+}
